b5b74436cc6653afdb053815fb211c643f8bf433
Sakebul-islam <mdsakebul66@gmail.com>
2023-01-28 22:38:16 +0600

    Convert GET HTTP request to async await function

--- src/App.js
+++ src/App.js
@@ -2,26 +2,23 @@ import React, { Fragment, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+const url = 'https://swapi.dev/api/films/';
 
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films/')
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        const transformedMovies = data.results.map(movieData => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  const fetchMoviesHandler = async url => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   };
 
   return (
